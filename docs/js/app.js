5148aca74118d2c62cb5f53e9a1c3bc5aefe16af
RishabhDharmani <55529677+RishabhDharmani@users.noreply.github.com>
2020-06-17 19:09:43 +0530

    Add files via upload

---
+++ docs/js/app.js
@@ -0,0 +1,193 @@
+
+
+//////////////////////// Global Variables for Functions /////////////////////////////
+
+var gumStream; 						//stream from getUserMedia()
+var input;							//MediaStreamAudioSourceNode we'll be recording
+var bufferLength;					//Number of frequency bins
+var dataArray = [];					//Stores all the frequency data of all frames 					
+var pause;							//Start and stop audio frame collection
+var jsonStoring;					//Store and send as JSON object
+var websocket;
+
+
+////////////////////////// AudioContext variables //////////////////////////////////
+
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext; //audio context to help us record
+var analyser;	  //analyser for getting frequency data	
+
+////////////////// Assign buttons to respective variables /////////////////////////
+
+var recordButton = document.getElementById("recordButton");
+var stopButton = document.getElementById("stopButton");
+var pauseButton = document.getElementById("pauseButton");
+
+////////////////////// Add events to those 3 buttons //////////////////////////////
+
+recordButton.addEventListener("click", startRecording);
+stopButton.addEventListener("click", stopRecording);
+pauseButton.addEventListener("click", pauseRecording);
+
+////////////////////// Start Recording Function //////////////////////////////////
+
+function startRecording() {
+
+	console.log("recordButton clicked");
+
+	//Simple constraints object
+    var constraints = { audio: true, video:false }
+
+    //Disable the record button until we get a success or fail from getUserMedia()
+	recordButton.disabled = true;
+	stopButton.disabled = false;
+	pauseButton.disabled = false;
+
+    //Using the standard promise based getUserMedia()
+	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+
+		console.log("getUserMedia() success");
+
+		/*
+			create an audio context after getUserMedia is called
+			create analyser node for frequency data collection
+		*/
+		audioContext = new AudioContext();
+		analyser = audioContext.createAnalyser();
+
+		//assign to gumStream for later use
+		gumStream = stream;
+
+		websocket = new WebSocket("ws://localhost:3030");
+
+		//use the stream
+		input = audioContext.createMediaStreamSource(stream);
+
+		//connecting input to analyser node
+		input.connect(analyser);
+
+		/*
+			frequency will be collected using fast fourier transform for every frame
+			set the size of bins for collecting frequency
+			the total bins are half the fft size
+			bufferLength is the total bins
+		*/
+		analyser.fftSize = 1024;
+		bufferLength = analyser.frequencyBinCount;
+
+		/*	
+			start collection of frequency data of frames
+			start the visualizer 
+		*/	
+		pause = false;
+		frameLooper();
+
+		console.log("Recording started");
+	})
+}
+
+/////////////// Record frequency data, Calling visualizer function ///////////////
+
+function frameLooper(){
+
+	//if paused/stopped will not collect data
+	if(pause) return;
+
+	//start taking frames as inputs		
+	window.requestAnimationFrame(frameLooper);
+
+	//array to store frequency data of each frame
+	fbc_array = new Uint8Array(analyser.frequencyBinCount);
+	analyser.getByteFrequencyData(fbc_array);
+
+	if (websocket.readyState === WebSocket.OPEN){
+      	console.log("Connection established");
+		websocket.send(fbc_array.buffer);
+   }
+
+	//creating the visualizer
+	visualizer(fbc_array);
+	
+}
+
+////////////////////// Pause and Resume function /////////////////////////////////
+
+function pauseRecording(){
+	console.log("pauseButton clicked rec.recording=");
+	if (pause ===false){
+		//pause
+		pauseButton.innerHTML="Resume";
+		pause = true;
+	}
+	else{
+
+		pauseButton.innerHTML="Pause";
+		pause= false;
+		frameLooper();
+	}
+}
+
+////////////////////// Stop Recording and Store Data /////////////////////////////
+
+function stopRecording() {
+	console.log("stopButton clicked");
+
+	//disable the stop button, enable the record to allow for new recordings
+	stopButton.disabled = true;
+	recordButton.disabled = false;
+	pauseButton.disabled = true;
+
+	//reset button just in case the recording is stopped while paused
+	pauseButton.innerHTML="Pause";
+
+	//pause the input of audio frames and visualizer
+	pause = true;
+
+
+	//stop microphone access
+	gumStream.getAudioTracks()[0].stop();
+
+	if (websocket.readyState === WebSocket.OPEN){
+      websocket.close( );
+      console.log("Websocket Closed");
+   }
+}
+
+
+// ///////////////////////// Visualizer Function ///////////////////////////////////
+
+function visualizer(fbc_array){
+
+	// Taking Canvas for applying visualizer
+	var canvas = document.getElementById("slide");
+	var ctx = canvas.getContext("2d");
+
+	//defines the width and height of the visualizer
+	var WIDTH = canvas.width;
+   	var HEIGHT = canvas.height;
+
+   	//define width and height of each bar of frequency
+   	var barWidth = (WIDTH/bufferLength);
+   	var barHeight;
+   	var x = 0;
+
+   	//initial colour black
+   	ctx.fillStyle = "#000";
+   	ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+   	//loop for each frequency bar height
+    for (var i = 0; i < bufferLength; i++) {
+	    barHeight = fbc_array[i];
+        
+        //applying different shades of colors to bars to make it look better
+    	var r = barHeight + (25 * (i/bufferLength));
+        var g = 250 * (i/bufferLength);
+        var b = 50;
+
+        //filling according to set colors
+        ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+        ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+
+        x += barWidth;
+      }
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const WebSocket = require('ws');
+var count=0;
+
+app.use(express.static("docs"));
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`listening http://localhost:${port}`);
+});
+
+
+const socketServer = new WebSocket.Server({port: 3030});
+
+socketServer.on('connection', function(data){
+
+	data.on('message', function(message){
+
+		var bytearray = new Uint8Array(message);
+		console.log(bytearray);
+	});
+
+	
+});

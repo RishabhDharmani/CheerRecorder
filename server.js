bf7ecdf516085c58962dfd8395f66a9e7034bee3
RishabhDharmani <55529677+RishabhDharmani@users.noreply.github.com>
2020-06-18 11:53:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const WebSocket = require('ws');
-var count=0;
 
-app.use(express.static("docs"));
 
-const port = 3000;
+const express = require('express');					// for using express framework
+const path = require('path');						// for working with directories and paths
+const WebSocket = require('ws');					// for websocket connections
+
+const app = express();							// app variable for using express
+const port = 3000;							// defining port		
+const socketServer = new WebSocket.Server({port: 3030});		// socket server for recieving from client
+
+//defining the static files folder
+app.use(express.static("docs"));			
+
+//checking fo connection success
 app.listen(port, () => {
   console.log(`listening http://localhost:${port}`);
 });
 
-
-const socketServer = new WebSocket.Server({port: 3030});
-
+// server recieves when connection is made 
 socketServer.on('connection', function(data){
 
 	data.on('message', function(message){
 
+		//variable to store arrays of data recieved
 		var bytearray = new Uint8Array(message);
-		console.log(bytearray);
-	});
-
-	
+		//console.log(bytearray);
+	});	
 });
